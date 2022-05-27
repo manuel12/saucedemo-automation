91f6daa16cbc307c99d530592f7f8e663aec2858
Manuel <manuelpinedacabeza@gmail.com>
2022-05-27 15:09:49 +0100

    Added comments, sortAscending, sortDescending and getNumValuesFromPricesArr functions and renamed createArrInRange to createNumArrInRange.

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,6 +1,38 @@
-export const createArrInRange = (range, minVal = 0) => {
+export const createNumArrInRange = (range, minVal = 1) => {
+  /**
+   * Create a numerical array with using the range param
+   * as an inclusive delimitator.
+   */
   let arrayInRange = [];
 
   for (let i = minVal; i <= range; i++) arrayInRange.push(i);
   return arrayInRange;
+};
+
+export const sortAscending = function (a, b) {
+  /**
+   * Function to pass as param to array.sort.
+   */
+  return a - b;
+};
+
+export const sortDescending = function (a, b) {
+  /**
+   * Function to pass as param to array.sort.
+   */
+  return b - a;
+};
+
+export const getNumValuesFromPricesArr = (priceArr) => {
+  /**
+   * Returns an array of numerical values from an array of
+   * string prices: ["$5.99", "$14.99"] => [5.99, 14.99].
+   */
+  let numValues = [];
+  for (const itemValueStr of priceArr) {
+    const cleanedItemValueStr = itemValueStr.replace("$", "");
+    const itemNum = Number(cleanedItemValueStr);
+    numValues.push(itemNum);
+  }
+  return numValues;
 };
